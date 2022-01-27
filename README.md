60f2a24ea665595b2fc7c9a70777de8ba73deb1a
Glen <93544347+glenhodges@users.noreply.github.com>
2022-01-27 07:30:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Signature Generator 5000
+
+To run locally:
+
+```
+yarn
+
+yarn start
+```
+
+## Deploy
+This app is node based and deployed to Heroku. To deploy, first go to a browser and and log into Creative Guild's Heroku account, creds are in Last Pass. 
+
+Next, cd to your project in a terminal and type `heroku login` and hit enter. Follow the steps. 
+
+Finally, when you are ready to deploy, type `git push heroku master`. That's it!
+
+## Author
+Glen Hodges - ghodges@creativeguild.ca
---
+++ app/Controllers/Http/SignatureController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+class SignatureController {
+    async start({request, response, view, params}) {
+        return view.render( 'start' )
+    }
+
+    async generate({request, view}) {
+
+        let { full_name, credentials, role, email, phone, extension, mobile, mobile_clean } = request.post()
+
+        // These are the properties you can access directly in the template via {{prop name}}
+        let data = {
+            full_name: full_name || 'Dr. So Fun',
+            credentials: credentials || 'creds here',
+            role: role || 'Director of Fun',
+            extension: extension,
+            email: email || 'example@creativeguild.ca',
+            phone: phone || null,
+            phone_clean: phone ? phone.replace(/\D/g,'') : null || null,
+            mobile: mobile || null,
+            mobile_clean: mobile ? mobile.replace(/\D/g,'') : null || null,
+            color_primary: '#2e9ef7',
+            color_secondary: '#7dc242',
+            color_light: 'rgb(130, 130, 130)'
+        }
+
+        // This is the template to render
+        let signature = view.render( 'signature', data )
+
+        view.share({
+            full_name: full_name || 'Dr. So Fun',
+            signature: signature
+        })
+
+        return view.render( 'generate' )
+    }
+}
+
+module.exports = SignatureController
